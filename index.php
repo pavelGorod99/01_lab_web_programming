f21680e03d1f34052d45db177c7b97602f2bd59c
Name <pavel.gorodetchi@gmail.com>
2022-03-26 22:46:22 +0100

    updated laboratory

--- index.php
+++ index.php
@@ -10,24 +10,75 @@
 
     Flight::register('todoDao', 'TodoDao');
 
-    Flight::route("GET /get_all", function () {
-        Flight::json(Flight::todoDao()->get_all());
+    Flight::route("GET /get_all/@table", function ($table) {
+        Flight::json(Flight::todoDao()->get_all($table));
     });
 
-    Flight::route("GET /get_by_id/@id", function($id) {
-        Flight::json(Flight::todoDao()->get_by_id($id));
+    Flight::route("GET /get_by_id/@table/@id", function($table, $id) {
+        Flight::json(Flight::todoDao()->get_by_id($table, $id));
     });
 
-    Flight::route("POST /add/@description/@created", function($description, $created) {
-        Flight::json(Flight::todoDao()->add($description, $created));
+    Flight::route("POST /add_author/@name/@nr_books", function($name, $nr_books) {
+        Flight::json(Flight::todoDao()->add_author($name, $nr_books));
     });
 
-    Flight::route("PUT /update/@id/@description/@created", function($id, $description, $created) {
-        Flight::json(Flight::todoDao()->update($id, $description, $created));
+    Flight::route("POST /add_book/@title/@id_publishing_house/@id_author/@year/@nr_pages/@price", function($title, $id_publishing_house, $id_author, $year, $nr_pages, $price) {
+        Flight::json(Flight::todoDao()->add_book($title, $id_publishing_house, $id_author, $year, $nr_pages, $price));
     });
 
-    Flight::route("DELETE /delete/@id", function($id) {
-        Flight::json(Flight::todoDao()->delete($id));
+    Flight::route("POST /add_publishing_house/@name/@nr_books/@address/@phone_number", function($name, $nr_books, $address, $phone_number) {
+        Flight::json(Flight::todoDao()->add_publishing_house($name, $nr_books, $address, $phone_number));
+    });
+
+    Flight::route("POST /add_reader/@name/@surname/@address/@phone_number", function($name, $surname, $address, $phone_number) {
+        Flight::json(Flight::todoDao()->add_reader($name, $surname, $address, $phone_number));
+    });
+
+    Flight::route("POST /add_reading_sheet/@id_book/@id_reader/@loan_date", function($id_book, $id_reader, $loan_date) {
+        Flight::json(Flight::todoDao()->add_reading_sheet($id_book, $id_reader, $loan_date));
+    });
+
+    Flight::route("DELETE /delete_from_table_by_id/@table/@id", function($table, $id) {
+        Flight::json(Flight::todoDao()->delete_from_table_by_id($table, $id));
+    });
+
+    Flight::route("PUT /update_book/@id/@title/@id_publishing_house/@id_author/@year/@nr_pages/@price/@image_path", function($id, $title, $id_publishing_house, $id_author, $year, $nr_pages, $price, $image_path) {
+        
+        Flight::json(Flight::todoDao()->update_book($id, $title, $id_publishing_house, $id_author, $year, $nr_pages, $price, $image_path));
+    });
+
+    Flight::route("PUT /update_table/@table/@parameters", function($table, $parameters) {
+
+        $parameters = explode("|", $parameters);
+
+        $_ARRAY_0 = array();
+        $_ARRAY_2 = array();
+
+        switch($table) {
+
+            case 'author': 
+                $_ARRAY_0 = ['_ID_AUTHOR', '_NAME', '_NR_BOOKS'];
+                $_ARRAY_2 = ['Id_Author', 'Name', 'Nr_Books'];
+            break;
+            case 'book':
+                $_ARRAY_0 = ['_ID_BOOK', '_TITLE', '_ID_PUBLISHING_HOUSE', '_ID_AUTHOR', '_YEAR', '_NR_PAGES', '_PRICE'];
+                $_ARRAY_2 = ['Id_Book', 'Title', 'Id_publishing_house', 'Id_Author', 'Year', 'Nr_Pages', 'Price'];
+            break;
+            case 'publishing_house':
+                $_ARRAY_0 = ['_ID_PUBLISHING_HOUSE', '_NAME', '_NR_BOOKS', '_ADDRESS', '_PHONE_NUMBER'];
+                $_ARRAY_2 = ['Id_publishing_house', 'Name', 'Nr_Books', 'Address', 'Phone_number'];
+            break;
+            case 'reader':
+                $_ARRAY_0 = ['_ID_READER', '_NAME', '_SURNMAE', '_ADDRESS', '_PHONE_NUMBER'];
+                $_ARRAY_2 = ['Id_Reader', 'Name', 'Surname', 'Address', 'Phone_number'];
+            break;
+            case 'reading_sheet':
+                $_ARRAY_0 = ['_ID_SHEET', '_ID_BOOK', '_ID_READER', '_LOAN_DATE'];
+                $_ARRAY_2 = ['Id_Sheet', 'Id_Book', 'Id_Reader', 'Loan_date'];
+            break;
+        }
+        
+        Flight::json(Flight::todoDao()->update_table($table, $_ARRAY_0, $parameters, $_ARRAY_2));
     });
 
     Flight::start();
--- rest/dao/TodoDao.class.php
+++ rest/dao/TodoDao.class.php
@@ -11,39 +11,108 @@ class TodoDao {
         $_servername = "localhost";
         $_USERNAME = "root";
         $_PASSWORD = "";
-        $_SCHEMA = "todo";
+        $_SCHEMA = "library";
         $this->CONNECTION = new PDO("mysql:host=$_servername;dbname=$_SCHEMA", $_USERNAME, $_PASSWORD);
         $this->CONNECTION->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
-    public function get_by_id($_ID) {
+    public function get_by_id($_TABLE, $_ID) {
 
-        $_STMT = $this->CONNECTION->prepare("SELECT * FROM todos WHERE id=" . $_ID);
+        $_STMT = $this->CONNECTION->prepare("SELECT * FROM " . $_TABLE . " WHERE Id_Author=" . $_ID);
         $_STMT->execute();
         return $_STMT->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function get_all() {
-        $_STMT = $this->CONNECTION->prepare("SELECT * FROM todos");
+    public function get_all($_TABLE) {
+        $_STMT = $this->CONNECTION->prepare("SELECT * FROM " . $_TABLE);
         $_STMT->execute();
         return $_STMT->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function add($_DESCRIPTION, $_CREATED) {
-        $_STMT = $this->CONNECTION->prepare("INSERT INTO todos (description, created) VALUES (:description, :created)");
-        $_STMT->execute(['description' => $_DESCRIPTION, 'created' => $_CREATED]);
+    public function add_author($_NAME, $_NR_BOOKS) {
+        $_STMT = $this->CONNECTION->prepare("INSERT INTO author (Name, Nr_Books) VALUES (:_NAME, :_NR_BOOKS)");
+        $_STMT->execute(['_NAME' => $_NAME, '_NR_BOOKS' => $_NR_BOOKS]);
     }
 
-    public function delete ($_ID) {
-        $_STMT = $this->CONNECTION->prepare("DELETE FROM todos WHERE id=:id");
-        $_STMT->bindParam(':id', $_ID);
+    public function add_book($_TITLE, $_ID_PUBLISHING_HOUSE, $_ID_AUTHOR, $_YEAR, $_NR_PAGES, $_PRICE) {
+        $_STMT = $this->CONNECTION->prepare("INSERT INTO book (Title, Id_publishing_house, Id_Author, `Year`, Nr_Pages, Price) 
+                                             VALUES (:_TITLE, :_ID_PUBLISHING_HOUSE, :_ID_AUTHOR, :_YEAR, :_NR_PAGES, :_PRICE)");
+        $_STMT->execute([   '_TITLE' => $_TITLE, 
+                            '_ID_PUBLISHING_HOUSE' => $_ID_PUBLISHING_HOUSE,
+                            '_ID_AUTHOR' => $_ID_AUTHOR,
+                            '_YEAR' => $_YEAR,
+                            '_NR_PAGES' => $_NR_PAGES,
+                            '_PRICE' => $_PRICE]);
+    }
+
+    public function add_publishing_house($_NAME, $_NR_BOOKS, $_ADDRESS, $_PHONE_NUMBER) {
+        $_STMT = $this->CONNECTION->prepare("INSERT INTO publishing_house (Name, Nr_Books, Address, Phone_number) 
+                                             VALUES (:_NAME, :_NR_BOOKS, :_ADDRESS, :_PHONE_NUMBER)");
+        $_STMT->execute([   '_NAME' => $_NAME, 
+                            '_NR_BOOKS' => $_NR_BOOKS,
+                            '_ADDRESS' => $_ADDRESS,
+                            '_PHONE_NUMBER' => $_PHONE_NUMBER]);
+    }
+
+    public function add_reader($_NAME, $_SURNAME, $_ADDRESS, $_PHONE_NUMBER) {
+        $_STMT = $this->CONNECTION->prepare("INSERT INTO reader (Name, Surname, Address, Phone_number) 
+                                             VALUES (:_NAME, :_SURNAME, :_ADDRESS, :_PHONE_NUMBER)");
+        $_STMT->execute([   '_NAME' => $_NAME, 
+                            '_SURNAME' => $_SURNAME,
+                            '_ADDRESS' => $_ADDRESS,
+                            '_PHONE_NUMBER' => $_PHONE_NUMBER]);
+    }
+
+    public function add_reading_sheet($_ID_BOOK, $_ID_READER, $_LOAN_DATE) {
+        $_STMT = $this->CONNECTION->prepare("INSERT INTO reading_sheet (Id_Book, Id_Reader, Loan_date) 
+                                             VALUES (:_ID_BOOK, :_ID_READER, :_LOAN_DATE)");
+        $_STMT->execute([   '_ID_BOOK' => $_ID_BOOK, 
+                            '_ID_READER' => $_ID_READER,
+                            '_LOAN_DATE' => $_LOAN_DATE]);
+    }
+
+    public function delete_from_table_by_id ($_TABLE, $_ID) {
+        $_STMT = $this->CONNECTION->prepare("DELETE FROM " . $_TABLE . " WHERE Id_Book=:_ID");
+        $_STMT->bindParam(':_ID', $_ID);
         $_STMT->execute();
     }
 
-    public function update ($_ID, $_DESCRIPTION, $_CREATED) {
-        // $_ID=intval($_ID);
-        $_STMT = $this->CONNECTION->prepare("UPDATE todos SET description=:description, created=:created WHERE id=:id");
-        $_STMT->execute(['id' => $_ID, 'description' => $_DESCRIPTION, 'created' => $_CREATED]);
+    public function update_table($_TABLE, $_ARRAY_0, $_ARRAY_1, $_ARRAY_2) {
+
+        $_QUERY = "";
+        for ($i = 1; $i < count($_ARRAY_1); $i++) {
+            
+            $_QUERY .= $_ARRAY_2[$i] . '=:' . $_ARRAY_0[$i];
+            if ($i != count($_ARRAY_1) - 1)
+                $_QUERY .= ',';
+        }
+         
+        $_STMT = $this->CONNECTION->prepare('UPDATE ' . $_TABLE . ' SET ' . $_QUERY . ' WHERE ' . $_ARRAY_2[0] . '=:' . $_ARRAY_0[0]);
+
+        $_QUERY = array();
+        for ($i = 0; $i < count($_ARRAY_1); $i++)
+            $_QUERY[$_ARRAY_0[$i]] = $_ARRAY_1[$i];
+
+        $_STMT->execute($_QUERY); 
+    }
+
+    public function update_book ($_ID, $_TITLE, $_ID_PUBLISHING_HOUSE, $_ID_AUTHOR, $_YEAR, $_NR_PAGES, $_PRICE, $_IMAGE_PATH) {
+        $_STMT = $this->CONNECTION->prepare("UPDATE book SET    Title=:_TITLE, 
+                                                                Id_publishing_house=:_ID_PUBLISHING_HOUSE,
+                                                                Id_Author=:_ID_AUTHOR,
+                                                                `Year`=:_YEAR,
+                                                                Nr_Pages=:_NR_PAGES,
+                                                                Price=:_PRICE,
+                                                                Image_Path=:_IMAGE_PATH WHERE Id_Book=:_ID");
+        $_STMT->execute([   '_ID' => $_ID, 
+                            '_TITLE' => $_TITLE, 
+                            '_ID_PUBLISHING_HOUSE' => $_ID_PUBLISHING_HOUSE,
+                            '_ID_AUTHOR' => $_ID_AUTHOR,
+                            '_YEAR' => $_YEAR,
+                            '_NR_PAGES' => $_NR_PAGES,
+                            '_PRICE' => $_PRICE,
+                            '_IMAGE_PATH' => $_IMAGE_PATH
+                        ]);
     }
 }
 
